e2bc61081f38fc9b7940d6faa3f18c1a94375380
Teerasej <teerasej@gmail.com>
2021-04-19 21:10:16 +0700

    สร้าง class เป็นตัวแทนของข้อมูลผู้ติดต่อ

---
+++ ContactModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace LocalContact
+{
+    public class ContactModel
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public ContactModel(string name, string phoneNumber)
+        {
+            this.Name = name;
+            this.PhoneNumber = phoneNumber;
+        }
+    }
+}
