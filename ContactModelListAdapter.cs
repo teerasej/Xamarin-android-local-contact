d39c88bf0e8854f75b3542d397e908c14664990d
Teerasej <teerasej@gmail.com>
2021-04-20 22:00:52 +0700

    สร้างใช้งาน ListAdapter - รับ Activity และข้อมูลมาเก็บใช้งาน - implement การทำงานต่างๆ

--- ContactModelListAdapter.cs
+++ ContactModelListAdapter.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Android.App;
 using Android.Views;
 using Android.Widget;
 
@@ -6,23 +8,33 @@ namespace LocalContact
 {
     public class ContactModelListAdapter : BaseAdapter<ContactModel>
     {
+        ContactModel[] contacts;
+        Activity context;
 
-        public ContactModelListAdapter()
+        public ContactModelListAdapter(Activity context, ContactModel[] contacts)
         {
+            this.context = context;
+            this.contacts = contacts;
         }
 
-        public override ContactModel this[int position] => throw new NotImplementedException();
+        public override ContactModel this[int position] => this.contacts[position];
 
-        public override int Count => throw new NotImplementedException();
+        public override int Count => this.contacts.Length;
 
         public override long GetItemId(int position)
         {
-            throw new NotImplementedException();
+            return position;
         }
 
         public override View GetView(int position, View convertView, ViewGroup parent)
         {
-            throw new NotImplementedException();
+            View view = convertView; // re-use an existing view, if one is available
+            if (view == null) // otherwise create a new one
+                view = context.LayoutInflater.Inflate(Resource.Layout.list_item_contact, null);
+
+            view.FindViewById<TextView>(Resource.Id.textName).Text = contacts[position].Name;
+            view.FindViewById<TextView>(Resource.Id.textPhoneNumber).Text = contacts[position].PhoneNumber;
+            return view;
         }
     }
 }
