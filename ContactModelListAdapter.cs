fbf5e2555e3249989636036227ef7c89c80d1009
Teerasej <teerasej@gmail.com>
2021-04-20 20:40:09 +0700

    สร้าง ListAdapter สำหรับข้อมูล ContactModel

---
+++ ContactModelListAdapter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Android.Views;
+using Android.Widget;
+
+namespace LocalContact
+{
+    public class ContactModelListAdapter : BaseAdapter<ContactModel>
+    {
+
+        public ContactModelListAdapter()
+        {
+        }
+
+        public override ContactModel this[int position] => throw new NotImplementedException();
+
+        public override int Count => throw new NotImplementedException();
+
+        public override long GetItemId(int position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
